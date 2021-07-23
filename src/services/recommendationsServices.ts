b775fe65384433d90fafa2d05840a3c490c1a5b4
Yann <yann.melo@gmail.com>
2021-07-23 12:07:16 -0300

    If condition dont match returns random song on random route, fixed!

--- src/services/recommendationsServices.ts
+++ src/services/recommendationsServices.ts
@@ -3,15 +3,22 @@ import { getAll } from "../repositories/recommendationsRepository";
 export async function selectRandomSongRepository() {
     const result = await getAll();   
     const randomize = ((Math.random()*100));
-    if(result.length === 0) return null
-    let filteredArray; 
+    if(result.length === 0) return null 
     if(randomize > 30){
-        filteredArray = result.filter((e)=>e.score > 10);
+        const filteredArray = result.filter((e)=>e.score > 10);
         const randomizeSong = Math.floor(Math.random() * filteredArray.length);
-        return filteredArray[randomizeSong];
+        if(filteredArray.length === 0){
+            return result[randomizeSong];
+        }else{
+            return filteredArray[randomizeSong];
+        }
     }else{
-        filteredArray = result.filter((e)=>e.score <= 10);
+        const filteredArray = result.filter((e)=>e.score <= 10);
         const randomizeSong = Math.floor(Math.random() * filteredArray.length);
-        return filteredArray[randomizeSong];
+        if(filteredArray.length === 0){
+            return result[randomizeSong];
+        }else{
+            return filteredArray[randomizeSong];
+        }
     }
 }
